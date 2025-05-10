ac1d60e5188eb89722f0175a25582c1e15f44995
Facymx <89006646+Yakult-996@users.noreply.github.com>
2025-05-10 21:06:35 +0800

    Add files via upload

--- App/Menu.c
+++ App/Menu.c
@@ -19,8 +19,8 @@ void AboutMessage(void)
 {
 	OLED_Printf(8, 0, OLED_8X16, "欢迎使用万年历");
 	OLED_Printf(0, 16, OLED_8X16, "版本：V1.0");
-	OLED_Printf(0, 32, OLED_8X16, "作者：牟峻永");
-	OLED_Printf(0, 48, OLED_8X16, "学号：2220222790");
+	OLED_Printf(0, 32, OLED_8X16, "作者：XXX");
+	OLED_Printf(0, 48, OLED_8X16, "学号：XXXXXXXXXX");
 	OLED_Update();
 	Delay_ms(1500);
 	OLED_Clear();
